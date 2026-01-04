af467603a1e5213c012e7637fbb92479da45bb84
kavyabhardwaj122317-arch <kavyabhardwaj122317@gmail.com>
2026-01-04 20:08:16 +0530

    update

--- server/server.js
+++ server/server.js
@@ -1,9 +1,6 @@
 
 console.log("hello world")
-
-
 const buttons = document.querySelectorAll("button-container");
-
 buttons.forEach(button => {
     button.addEventListener("onclick", () => {
         const sem = button.getAttribute("data-sem");
