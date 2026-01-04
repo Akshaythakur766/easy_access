89a4627b042a3c685094278485ff3dc4a23a8ee7
kavyabhardwaj122317-arch <kavyabhardwaj122317@gmail.com>
2026-01-04 18:02:28 +0530

    add : links

---
+++ server/server.js
@@ -0,0 +1,9 @@
+
+const buttons = document.querySelectorAll("button-container");
+
+buttons.forEach(button => {
+    button.addEventListener("onclick", () => {
+        const sem = button.getAttribute("data-sem");
+       window.location.href = `sem${sem}.html`;
+    });
+});
